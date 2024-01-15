12e2a6b2765814a870b3d7f91dc26e3caa70d161
Bodhi van Baardewijk <bodhivanbaardewijk@hotmail.com>
2024-01-16 00:00:39 +0100

    Improve main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ GPIO.setup(LATCH_PIN, GPIO.OUT)
 
 # Function to shift data into the shift register
 def shift_data(data):
-    for bit in range(8):
+    for bit in range(16):
         GPIO.output(DATA_PIN, (data >> bit) & 1)
         GPIO.output(CLOCK_PIN, GPIO.HIGH)
         GPIO.output(CLOCK_PIN, GPIO.LOW)
@@ -23,9 +23,10 @@ def shift_data(data):
     GPIO.output(LATCH_PIN, GPIO.LOW)
 
 # Function to toggle the relay state
-def toggle_relay_state(relay_button):
-    relay_button.toggle()
-    relay_state = 0b00000000
+def toggle_relay_state(index):
+    rb = relay_buttons[index]
+    rb.toggle()
+    relay_state = 0b0000000000000000
     for i, button in enumerate(relay_buttons):
         if button.value:
             relay_state |= (1 << i)
@@ -39,13 +40,17 @@ relay_box = Box(app, layout="grid")
 
 # Create the relay buttons
 relay_buttons = []
-for i in range(8):
-    relay_button = PushButton(relay_box, text="Relay {}".format(i+1), grid=[i, 0], command=lambda button=relay_button: toggle_relay_state(button))
+for i in range(16):
+    relay_button = PushButton(relay_box, text="Relay {}".format(i+1), grid=[i, 0], command=lambda: toggle_relay_state(i))
     relay_buttons.append(relay_button)
-
-
+    
 # Clean up GPIO after the app is closed
-app.when_closed = GPIO.cleanup
+def on_close():
+    GPIO.cleanup()
+    app.destroy()
+
+# Set the app close event handler
+app.when_closed = on_close
 
 # Run the app
 app.display()
